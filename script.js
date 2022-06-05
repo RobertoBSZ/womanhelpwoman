581d93e86c2f2c6a3aa70303e31439fdcecaae41
Roberto Bastos Souza <103335050+RobertoBSZ@users.noreply.github.com>
2022-06-05 16:51:23 -0300

    Atualizando o script

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ function localizar() {
 }
 
 function showPosition(pos) {
-    var lt = pos.coords.latitude
-    var lg = pos.coords.longitude
+    var lt = 50.33514898909946
+    var lg = 34.90596447104751
     document.getElementById("geo").innerHTML =
-        "Okhtyrka, Ucrânia" + "<br>" + "Latitude: " + 50.33514898909946 + " Longitude: " + 34.90596447104751
-    var map = L.map('map').setView([50.33514898909946, 34.90596447104751], 13);
-    var marker = L.marker([50.33514898909946, 34.90596447104751]).addTo(map);
+        "Okhtyrka, Ucrânia" + "<br>" + "Latitude: " + lt + " Longitude: " + lg
+    var map = L.map('map').setView([lt, lg], 13);
+    var marker = L.marker([lt, lg]).addTo(map);
 
     L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
